bd27147c427d24670b1d3813e865b2f0ac08a8e1
viethien <viethien_cntt@yahoo.com>
2018-07-01 17:07:50 +0700

    commit

--- Models/Category.cs
+++ Models/Category.cs
@@ -12,5 +12,7 @@ namespace Models
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
+
+        public string Image { get; set; }
     }
 }
