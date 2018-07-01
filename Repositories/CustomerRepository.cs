0e435cc4f76b76f7bd1d863ea31195a6998ae0ae
nguyenviethien <nguyenviethien@gmail.com>
2018-07-01 19:53:09 +0700

    update customer

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -21,7 +21,7 @@ namespace Repositories
         public List<Supplier> GetAll()
         {
             List<Supplier> result = new List<Supplier>();
-            string sqlQuery = string.Format("select * from {0}", tableName);
+            string sqlQuery = string.Format("select SupplierID from {0}", tableName);
             SqlCommand command = new SqlCommand(sqlQuery, _conn);
             SqlDataReader dataReader = command.ExecuteReader();
             if (dataReader.HasRows)
@@ -39,7 +39,7 @@ namespace Repositories
                         Country = dataReader["Country"].ToString(),
                         ContactTitle = dataReader["ContactTitle"].ToString(),
                         ContactName = dataReader["ContactName"].ToString(),
-                        CompanyName = dataReader["CompanyName"].ToString(),
+                        CompanyName = dataReader["CompanyName1"].ToString(),
                         City = dataReader["City"].ToString(),
                         Address = dataReader["Address"].ToString()
 
