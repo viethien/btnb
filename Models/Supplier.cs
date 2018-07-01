a8caad0e255d933962579639e3a4bcce91e46808
viethien <viethien_cntt@yahoo.com>
2018-07-01 16:44:22 +0700

    add comment

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Models
 {
+    /// <summary>
+    /// Supplier model
+    /// </summary>
     public class Supplier
     {
         public int SupplierID { get; set; }
